4976eadc7b4dbea891f40a7a59ebdb8027a00cfa
Ritu <ritu@gmail.com>
2018-10-02 12:41:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import * as BooksAPI from "./BooksAPI";
+import SearchPage from "./SearchPage";
+import { Route } from "react-router-dom";
+import MainPage from "./MainPage";
+import "./App.css";
+
+class BooksApp extends React.Component {
+  state = {
+    books: []
+  };
+
+componentDidMount() {
+  BooksAPI.getAll().then(books => {
+    this.setState({ books });
+  });
+}
+
+moveShelf = (book, shelf) => {
+  BooksAPI.update(book, shelf);
+
+  BooksAPI.getAll().then(books => {
+    this.setState({ books });
+  });
+};
+
+render() {
+  console.log(this.state.books);
+  return (
+    <div className="app">
+    <Route
+    exact
+    path="/"
+    render={() => (
+    <MainPage books={this.state.books} moveShelf={this.moveShelf} />
+    )}
+    />
+<Route
+path="/search"
+render={() => (
+  <SearchPage moveShelf={this.moveShelf} books={this.state.books} />
+  )}
+  />
+</div>
+);
+}
+}
+
+export default BooksApp;

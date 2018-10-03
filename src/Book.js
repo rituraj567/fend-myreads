b50923590b3e7f73fa5a02229a5ba8b6b91a0aa1
Ritu <ritu@gmail.com>
2018-10-03 21:01:22 +0530

    Changed bug in book.js

--- src/Book.js
+++ src/Book.js
@@ -34,7 +34,7 @@ class Book extends Component {
           </div>
         </div>
         <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">{this.props.authors ? this.props.authors.join(', ') : 'None'}</div>
+        <div className="book-authors">{this.props.authors.join(', ')}</div>
       </div>
     );
   }
